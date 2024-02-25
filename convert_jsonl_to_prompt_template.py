9b1d76a2e38ea496d84cae4ea25d4b404697c547
Zaid Omer <z2omer@uwaterloo.ca>
2024-02-24 20:22:04 -0500

    Finished description of keywords excluding save as, and potential save changes

---
+++ convert_jsonl_to_prompt_template.py
@@ -0,0 +1,26 @@
+# this code was inspired by the work done by Chris Hay
+# the sample code it was inspired from can be found here: https://github.com/chrishayuk/opl-train/blob/main/JSONL/test.py
+
+import jsonlines
+
+# jsonl input and output paths
+jsonl_input_paths = ["llama_prompts.jsonl"]
+jsonl_output_path = "train.jsonl"
+
+# Open the output JSONL file
+with open(jsonl_output_path, "w") as output_file:
+    json_writer = jsonlines.Writer(output_file)
+
+    for jsonl_input_path in jsonl_input_paths:
+        with open(jsonl_input_path, "r") as input_file:
+            json_reader = jsonlines.Reader(input_file)
+
+            for entry in json_reader:
+                try:
+                    # Combine prompt and completion into the desired format
+                    formatted_text = f"<s>[INST] {entry['prompt']} [/INST] {entry['completion']} </s>"
+
+                    # Write the formatted text as a JSONL line
+                    json_writer.write({"text": formatted_text})
+                except Exception as e:
+                    print(f"Error parsing JSON in {jsonl_input_path}:", entry, e)
